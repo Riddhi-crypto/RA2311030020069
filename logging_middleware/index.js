26f736d23386e1d3803ffbf7b7849103b788dd57
Riddhi Sahu <riddhi2379@gmail.com>
2026-05-02 11:00:18 +0530

    Implement logging function with external service integration
    Added a logging function to send logs to an external service.

--- logging_middleware/index.js
+++ logging_middleware/index.js
@@ -1,1 +1,21 @@
+import requests
 
+TOKEN = "PASTE_YOUR_ACCESS_TOKEN"
+
+def Log(stack, level, package, message):
+    url = "http://20.207.122.201/evaluation-service/logs"
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "stack": stack,
+        "level": level,
+        "package": package,
+        "message": message
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    print(response.json())
